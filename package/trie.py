b1d4b13830d4ecc2065d3ed65d40021a1a6357bf
陳代穎 <dying1020@gmail.com>
2019-04-08 18:15:39 +0800

    Add marisa trie package

--- package/trie.py
+++ package/trie.py
@@ -1,10 +1,15 @@
 import os
 import pickle
+from queue import Queue
+from marisa_trie import BytesTrie
+
+INT_BYTES = 4
+BYTEORDER = 'big'
 
 
 class Trie(object):
     def __init__(self):
-        self.root = {'children': {}}
+        self.root = {'children': {}, 'depth': 0}
         self.total = 0
         self.trie_file_path = os.path.join(
             os.path.dirname(__file__), "./Trie.pkl")
@@ -13,9 +18,11 @@ class Trie(object):
         """Build trie from a list of tokens
         """
         node = self.root
+        depth = 0
         for token in tokens:
+            depth += 1
             if token not in node['children']:
-                node['children'][token] = {'freq': 0, 'children': {}}
+                node['children'][token] = {'freq': 0, 'depth': depth, 'visit': False, 'value': tokens[:depth], 'children': {}}
             node = node['children'][token]
             node['freq'] += 1
             self.total += 1
@@ -29,8 +36,30 @@ class Trie(object):
                 return result
             node = node['children'][token]
             result = {**result, **node}
-            result['value'] += token
         return result
+
+    def bfs(self, min_depth, max_depth=-1):
+        """Generator for breadth-first search
+
+        Returns
+        -------
+        node : Trie tree node
+        """
+        queue = Queue()
+        queue.put(self.root)
+        while not queue.empty():
+            node = queue.get()
+
+            if max_depth != -1 and node['depth'] > max_depth: 
+                return
+            elif node['depth'] >= min_depth:
+                yield node
+
+            for child in node['children'].values():
+                if not child['visit']:
+                    child['visit'] = True
+                    queue.put(child)
+        return
 
     def visualize(self):
         self.visualize_util(self.root, '')
@@ -60,3 +89,13 @@ class Trie(object):
         queue = []
         self.visualize_util(self.root, '', console=False, queue=queue)
         return '\n'.join(queue)
+
+
+class BTrie(BytesTrie):
+    def build(self, trie):
+        trie = {k: v.to_bytes(INT_BYTES, byteorder=BYTEORDER) for k, v in trie.items()}
+        super().__init__(trie.items())
+        return self
+
+    def items(self, key):
+        return {k: int.from_bytes(v, byteorder=BYTEORDER) for k, v in super().items(key)}
